366bb38eb1c8eef1baa0c52455fab5aab21ee989
racheldean1 <racheldean1@arizona.edu>
2025-11-10 19:44:16 -0700

    Update predict_in_sample.m

--- src/predict_in_sample.m
+++ src/predict_in_sample.m
@@ -7,7 +7,7 @@ for k = 1:M
     for h = 1:K
         sea = sea + coef.alpha(h)*cos(2*pi*h*t/s) + coef.beta(h)*sin(2*pi*h*t/s);
     end
-    acc = coef.c + coef.d * t + sea; #changed
+    acc = coef.c + coef.d * t + sea; %changed
     for i = 1:N
         acc = acc + coef.a(i)*y(t-i);
     end
