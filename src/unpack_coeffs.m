0a7c18d9b9040821814d48e13247cc47fbab9621
racheldean1 <racheldean1@arizona.edu>
2025-11-10 20:26:44 -0700

    Update unpack_coeffs.m

--- src/unpack_coeffs.m
+++ src/unpack_coeffs.m
@@ -1,7 +1,7 @@
 function coef = unpack_coeffs(beta, N, K)
     coef.c     = beta(1);                 
     coef.d     = beta(2);                 % added for linear trend
-    coef.a     = beta(3 : 2+N);           % changed as a consequence of line 3
-    coef.alpha = beta(3+N : 2+N+K);       % changed as a consequence of line 4
-    coef.beta  = beta(3+N+K : 2+N+2*K);   % changed as a consequence of line 5
+    coef.a     = beta(3 : 2+N);           % changed as a consequence of line 3 (adjusting index)
+    coef.alpha = beta(3+N : 2+N+K);       % changed as a consequence of line 4 (adjusting index)
+    coef.beta  = beta(3+N+K : 2+N+2*K);   % changed as a consequence of line 5 (adjusting index)
 end
